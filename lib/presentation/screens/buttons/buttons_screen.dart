3783afc6caacce48b363611e0b13d4a616e05bee
jefry <jefrydep@gmail.com>
2023-03-10 18:44:27 -0500

    button personalizide

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -57,6 +57,7 @@ class _ButtonView extends StatelessWidget {
               icon: const Icon(Icons.account_box_outlined),
               label: const Text('Text Icon'),
             ),
+            CustomButton(),
             IconButton(
                 onPressed: () {},
                 icon: const Icon(Icons.app_registration_rounded)),
@@ -69,6 +70,28 @@ class _ButtonView extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text('Hola Mundo', style: TextStyle(color: Colors.white))),
         ),
       ),
     );
