5d3c85f3c45de7a394fcdc756289807c1183301d
jefry <jefrydep@gmail.com>
2023-03-15 15:05:03 -0500

    changin thmes

--- lib/presentation/screens/infiniti_scroll/infiniti_scrol_screen.dart
+++ lib/presentation/screens/infiniti_scroll/infiniti_scrol_screen.dart
@@ -1,11 +1,61 @@
-
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfinitiScrollScreen extends StatelessWidget {
+class InfinitiScrollScreen extends StatefulWidget {
   const InfinitiScrollScreen({super.key});
 
   @override
+  State<InfinitiScrollScreen> createState() => _InfinitiScrollScreenState();
+}
+
+class _InfinitiScrollScreenState extends State<InfinitiScrollScreen> {
+  List<int> imagesIds = [1, 2, 3, 4, 5, 6];
+  final ScrollController scrollController = ScrollController();
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) {}
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+    imagesIds.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Infiniti Scroll'),
+      ),
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          controller: scrollController,
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+                placeholder: const AssetImage('assets/Images/jar-loading.gif'),
+                image: NetworkImage(
+                    'https://picsum.photos/id/${imagesIds[index]}/500/300'));
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios),
+        onPressed: () => context.pop(),
+      ),
+    );
   }
 }
